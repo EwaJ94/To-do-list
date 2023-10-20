a1d9ac03f95e744549ebeecf0df3a14fe7dc8715
EwaJochymkova <ewa.jochymkova@seznam.cz>
2023-10-20 12:50:51 +0200

    label + checkbox

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ let saveTask = document.querySelector(".save")
 let deleteTask = document.querySelector(".delete")
 let openList = document.querySelector(".open-list")
 let removeAllTasks = document.querySelector(".remove-all")
-let myList = document.querySelector(".task")
-let myTasks = document.querySelector(".my-task")
+let myTasks = document.querySelector(".my-tasks")
 // let oneTask = document.querySelector(".one-task")
 
 // EXISTING LS CONDITION
@@ -17,14 +16,16 @@ if(localStorage.getItem("tasks") === null){
 // CREATE NEW TASK
 newTask.addEventListener("click", function(event){
 
-    let newLi = document.createElement("li")
-    let newText = document.createElement("input")
+    let newLabel = document.createElement("label")
+    let newCheck = document.createElement("input")
 
-    newText.type = "text"
-    newText.class = "one-task"
-    newLi.appendChild(newText)
-    myList.appendChild(newLi)
-    newText.focus()
+    newCheck.type = "checkbox"
+    newCheck.id = "one-check"
+    newLabel.setAttribute = ("for", "one-check")
+    newLabel.textContent = "new"
+    myTasks.appendChild(newCheck)
+    myTasks.appendChild(newLabel)
+    newLabel.focus()
     
 
     newText.addEventListener("change", function(event){
@@ -50,11 +51,10 @@ saveTask.addEventListener("click", function(){
 
 // OPEN TO DO LIST
 openList.addEventListener("click", function(){
-    // vyčištění okna a potom otevření listu
-
+   
     let myToDoList = localStorage.getItem("tasks")
     let myToDoListJ = JSON.parse(myToDoList)
-    // console.log(myToDoListJ)
+    
     myToDoListJ.forEach(function(oneTask){
 
         let newLi = document.createElement("li")
@@ -63,13 +63,10 @@ openList.addEventListener("click", function(){
         newText.type = "text"
         newText.class = "one-task"
         newText.textContent = oneTask
+
+
         newLi.appendChild(newText)
         myList.appendChild(newLi)
-
-        
-       
-        
-        
 
         
         
@@ -82,5 +79,6 @@ openList.addEventListener("click", function(){
 
 removeAllTasks.addEventListener("click", function(){
     localStorage.clear()
+    myList.remove()
 })
 
